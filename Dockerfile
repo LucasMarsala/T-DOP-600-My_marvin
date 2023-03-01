590e3cb5d891fcf85304eb22595526e522b15ebe
Lucas Marsala <lucas1.marsala@epitech.eu>
2023-03-01 16:35:22 +0100

    Add jenkins configuration file to take groovy script

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,10 @@ EXPOSE 8080
 
 Workdir /Marvin
 
-#ARG JAVA_OPTS
-
-#ENV JAVA_OPTS="-Djenkins.CLI.disabled=true ${JAVA_OPTS:-}"
-
 COPY --chown=jenkins:jenkins /plugins/plugins.txt /usr/share/jenkins/ref/plugins.txt
 
-COPY --chown=jenkins:jenkins /jobs/job_dsl.groovy /usr/share/jenkins/ref/init.groovy.d/job_dsl.groovy
+RUN jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
 
 COPY --chown=jenkins:jenkins /configuration/my_marvin.yml /var/jenkins_home/casc_configs/my_marvin.yml
 
-RUN jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
+COPY --chown=jenkins:jenkins /jobs/job_dsl.groovy .
--- jobs/job_dsl.groovy
+++ jobs/job_dsl.groovy
@@ -1,4 +1,25 @@
+import jenkins.model.Jenkins
+
 folder('Tools') {
     displayName('Tools')
     description('Folder for miscellaneous tools.')
+}
+
+freeStyleJob('Tools/clone-repository') {
+    description("Git URL of the repository to clone")
+    scm {
+        github('https://github.com/LucasMarsala/T-DOP-600-My_marvin.git', 'main')
+    }
+    publishers {
+    cleanWs {
+        cleanWhenAborted(true)
+        cleanWhenFailure(true)
+        cleanWhenNotBuilt(false)
+        cleanWhenSuccess(true)
+        cleanWhenUnstable(true)
+        deleteDirs(true)
+        notFailBuild(true)
+        disableDeferredWipeout(true)
+        }
+    }
 }
